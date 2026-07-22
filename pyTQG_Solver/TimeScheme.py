5c28e8c7528eb1896662b557621cf55830c501cc
personne30003 <122971649+personne30003@users.noreply.github.com>
2026-07-22 15:15:02 +0200

    Test et validation de la classe TimeScheme

--- pyTQG_Solver/TimeScheme.py
+++ pyTQG_Solver/TimeScheme.py
@@ -3,9 +3,7 @@ Tiré de mon solveur (en construction) QG barotrope de l'UE Projet Pro.
 
 Attention, cette classe n'a pas encore été testée et validée.
 
-Schémas implémentés (tous explicites) : Euler, Leapfrog (avec filtre d'asselin), RK4, Heun, RK3 SSP. 
-
-A faire : implémenter RK2 (pas indispensable, mais bon).
+Schémas implémentés (tous explicites) : Euler, Leapfrog (avec filtre d'asselin), RK4, Heun, RK2
 
 Fortement inspiré de Fluid2d (auteur, G.Roullet, LOPS). Oui, meme le nom est identique
 """
@@ -13,71 +11,85 @@ Fortement inspiré de Fluid2d (auteur, G.Roullet, LOPS). Oui, meme le nom est id
 import numpy as np
 import Params
 
+
 class TimeScheme:
     "Résout une équation de la forme du/dt=rhs(u, t, *args) par un schéma explicite"
     def __init__(self, params, rhs, u0):
-        self.SchemeList=\
-            {
+        self.__SchemeList = {
             'Euler' : self.Euler,
-            'Leapfrog' : self.Leapfrog,
+            'LeapFrog' : self.LeapFrog,
             'Heun' : self.Heun,
-            'RK3_SSP' : self.RK3_SSP,
+            'RK2' : self.RK2,
             'RK4' : self.RK4
-            }
-        if params.time_scheme not in self.SchemeList.keys():
-            raise ValueError(f"type must be in {self.SchemeList.keys()} (value {params.time_scheme})")
+        }
+
+
+        if params.time_scheme not in self.__SchemeList.keys():
+            raise ValueError(f"type must be in {self.__SchemeList.keys()} (value {params.time_scheme})")
             
         self.scheme_type=params.time_scheme
-        self.operator_scheme=self.SchemeList[self.scheme_type]
+        self.operator_scheme=self.__SchemeList[self.scheme_type]
         self.u=np.copy(u0)
+        #print(f"initialisation u={self.u}")
         self.rhs=rhs#Attention il s'agit d'une fonction
         self.t=0.0
         self.nb_it=0
         #paramètres pour schéma LeapFrog
         self.LeapFrog_init_scheme='Euler'
         self.LeapFrog_Asselin_coeff=0.05
-        self.LeapFrog_u_nm1=np.copy(u)
+        self.LeapFrog_u_old = np.copy(u0)
+        self.LeapFrog_u_f = np.zeros_like(u0)
+        self.LeapFrog_dt_old = None
         self.first_it = True
         
     def Step(self, dt, *args):
         self.nb_it+=1
         self.t+=dt
-        return self.operator_scheme(dt,  *args)
+        self.operator_scheme(dt,  *args)
+        self.LeapFrog_dt_old = dt
+        return self.u
         
     def Euler(self, dt, *args):
-        self.u = self.u + dt * self.rhs(u,self.t, *args)
-        return self.u
+        self.u = self.u + dt * self.rhs(self.u, self.t, *args)
+        #return self.u
         
-    def Leapfrog(self, dt,*args):
-        "A terminer"
+    def LeapFrog(self, dt,*args):
         u_n=np.copy(self.u)
         if self.first_it:
-            self.u=self.SchemeList[self.LeapFrog_init_scheme](dt,self.t,*args)#u_1
+            #print("first it")
+            self.__SchemeList[self.LeapFrog_init_scheme](dt,*args)#u_1
+            #self.RK2(dt, *args)
             self.first_it = False
+            self.LeapFrog_u_f
         else :
             #Pas valide si pas de temps non uniforme (c'est le cas dans mon solveur)
-            self.u=self.LeapFrog_u_nm1+dt*self.rhs(self.LeapFrog_u_nm1, self.t-dt, *args)#u_n+1
+            u_new=self.LeapFrog_u_old + 2.0*dt*self.rhs(self.u, self.t - self.LeapFrog_dt_old, *args)#u_n+1
+            
             #Application d'un filtre d'Asselin
-            u_n = u_n + self.LeapFrog_Asselin_coeff*(self.u + self.LeapFrog_u_nm1 - 2.0 * u_n)
-        self.LeapFrog_u_nm1 = u_n
-        return self.u
-
+            self.LeapFrog_u_f = self.u + self.LeapFrog_Asselin_coeff*(u_new + self.LeapFrog_u_old - 2.0 * self.u)
+            self.LeapFrog_u_old = self.LeapFrog_u_f.copy()
+            self.u = u_new.copy()
+        #self.LeapFrog_u_nm1 = u_n
+        #return self.u
+     
+    def RK2(self, dt, *args) : 
+        k1 = self.rhs(self.u, self.t, *args)
+        k2 = self.rhs(self.u + dt * k1, self.t+dt)
+        self.u = self.u + 0.5*dt*(k1+k2)
+        
     def RK4(self, dt, *args):
-        k1 = self.rhs(self.u, *args)
-        k2 = self.rhs(self.u+0.5*dt*k1, *args)
-        k3 = self.rhs(self.u+0.5*dt*k2, *args)
-        k4 = self.rhs(self.u+dt*k3, *args)
+        k1 = self.rhs(self.u, self.t, *args)
+        k2 = self.rhs(self.u+0.5*dt*k1, self.t+dt/2.0, *args)
+        k3 = self.rhs(self.u+0.5*dt*k2, self.t +dt/2.0, *args)
+        k4 = self.rhs(self.u+dt*k3, self.t+dt, *args)
 
         self.u = self.u + dt * (k1+2.0*k2+2.0*k3+k4)/6.0
-        return self.u
+        #return self.u
 
     def Heun(self, dt,*args,t=0.0):
         u_1=self.u+dt*self.rhs(self.u, self.t, *args)
-        self.u = self.u+dt/2.0*(self.rhs(u, self.t,*args)+self.rhs(u_1,self.t+dt, *args))
-        return self.u
-        
-    def RK3_SSP(self, dt, *args,t=0.0):
-        u1 = self.u+dt*self.rhs(self.u, self.t, *args)
-        u2 = (3.0/4.0)*self.u+(1.0/4.0)*(u1+dt*self.rhs(u1, self.t, *args))
-        self.u = (1.0/3.0)*self.u+(2.0/3.0)*(u2+dt*self.rhs(u2, self.t, *args))
-        return self.u
+        self.u = self.u+(dt/2.0)*(self.rhs(self.u, self.t,*args)+self.rhs(u_1,self.t+dt, *args))
+        #return self.u
+    
+    def get_Schemes(self) : 
+        return tuple(self.__SchemeList.keys())
