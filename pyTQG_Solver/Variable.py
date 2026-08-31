43c343615f676d76de9779a62325a84ab3a7e831
personne30003 <122971649+personne30003@users.noreply.github.com>
2026-08-31 12:09:26 +0200

    Validé la classe Variable

--- pyTQG_Solver/Variable.py
+++ pyTQG_Solver/Variable.py
@@ -1,19 +1,69 @@
 """
-Classe permettant de documenter les variables : nom, type (prognostique, diagnostique), attributs (sert pour la doc). Sert surtout pour State et la classe dédiée aux sorties (je sais pas comment l'appeler)
+Classe permettant de documenter les variables : nom, type (prognostique, diagnostique), attributs (sert pour la doc), valeur. Sert surtout pour State et la classe dédiée aux sorties (je sais pas comment l'appeler)
+
+Globalement valide. 
 """
 
-import dataclasses
+#import numpy as np
 
-
-@dataclasses.dataclass(frozen = True, slots = True)
 class Variable:
-    name : str
-    type : str#'diagnostic', 'prognostic'
-    units : str=''
-    integrated : bool = False
-    attrs : str = ''
 
-    def __post_init__(self):
-        if self.type not in ('prognostic', 'diagnostic'):
-            raise ValueError(f"attribute 'type' must be in ('prognostic', 'diagnostic'), current_value = {self.type} ")
+    def __init__(self,
+                 value = 0.0,
+                 type_var = 'prognostic',
+                 name = '',
+                 units = '',
+                 integrated = True,
+                 attrs = '') : 
+        self.value = value
+        if type_var not in ('prognostic', 'diagnostic'):
+            raise ValueError(f"type must be in ('prognostic, diagnostic'), not {type_var}")
+            
+        self.__type = type_var
+        self.__name = name
+        self.__units = units
+        self.__attrs = attrs
+        self.__integrated = integrated
     
+    def __eq__(self, other):
+        if not isinstance(other, Variable) : 
+            raise ValueError(f"cannor compare Variable with {type(other)}")
+            
+        msk = ((self.__name == other.name) and
+               (self.__type == other.type) and
+               (self.__units == other.units) and
+               (self.__integrated == other.integrated) and
+               (self.__attrs == other.attrs))
+        
+        return msk
+
+    def __repr__(self):
+        return f"""Variable : 
+        Immutables attributes : 
+        - name = {self.__name}
+        - type = {self.__type}
+        - units = {self.__units}
+        - integrated = {self.__integrated}
+        - attrs = {self.__attrs}
+        mutable attributes
+        - value = {self.value}"""
+    ##Accès aux attributs immutables (sauf valeur)
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def type(self):
+        return self.__type
+
+    @property
+    def units(self):
+        return self.__units
+
+    @property
+    def integrated(self):
+        return self.__integrated
+
+    @property
+    def attrs(self):
+        return self.__attrs
